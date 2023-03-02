8810795a7499f1d950389f8f0a69af268207d1ec
Alex Fun <alexanderthefun91@gmail.com>
2023-03-02 15:34:58 -0600

    phase1

--- Heist/Program.cs
+++ Heist/Program.cs
@@ -6,7 +6,14 @@ namespace Heist
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Plan your Heist!");
+            Console.WriteLine("Enter your team member's name.");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Enter {name}'s skill level, 1-10");
+            int skillLevel = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Enter {name}'s courage level, 1-10");
+            double courage = double.Parse(Console.ReadLine());
+            Console.WriteLine($"{name}'s skill level is {skillLevel} with a courage level of {courage}");
         }
     }
 }
