f82c7a73d2a6cefa8390df6cf6992e6b720d9b85
Alex Fun <alexanderthefun91@gmail.com>
2023-03-03 09:24:40 -0600

    phase 2

--- Heist/Program.cs
+++ Heist/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Heist
 {
@@ -6,7 +7,12 @@ namespace Heist
     {
         static void Main(string[] args)
         {
+
+            List<Member> HeistCrew = new List<Member>();
+
             Console.WriteLine("Plan your Heist!");
+
+            another:
             Console.WriteLine("Enter your team member's name.");
             string name = Console.ReadLine();
             Console.WriteLine($"Enter {name}'s skill level, 1-10");
@@ -14,6 +20,30 @@ namespace Heist
             Console.WriteLine($"Enter {name}'s courage level, 1-10");
             double courage = double.Parse(Console.ReadLine());
             Console.WriteLine($"{name}'s skill level is {skillLevel} with a courage level of {courage}");
+
+            Member member = new Member(name, skillLevel, courage);
+            HeistCrew.Add(member);
+
+            Console.WriteLine("Do you need add another member? (yes/no)");
+            string answer = Console.ReadLine().ToLower();
+            if (answer == "yes")
+            {
+                goto another;
+            }
+            else 
+            {
+                Console.WriteLine("---------------");
+                Console.Clear();
+                Console.WriteLine($"You have {HeistCrew.Count} team Members.");
+            }
+            foreach (var crewMem in HeistCrew)
+            {
+                Console.WriteLine(@$"
+Name: {crewMem.Name},
+Skill Level: {crewMem.SkillLevel},
+Courage Factor: {crewMem.Courage}");
+                Console.WriteLine("-------------");
+            }
         }
     }
 }
--- Heist/TeamMember.cs
+++ Heist/TeamMember.cs
@@ -4,6 +4,12 @@ namespace Heist
 {
     public class Member
     {
+        public Member(string newName, int newSkill, double newCourage)
+        {
+            Name = newName;
+            SkillLevel = newSkill;
+            Courage = newCourage;
+        }
         public string Name { get; set; }
         public int SkillLevel { get; set; }
         public double Courage { get; set; }
